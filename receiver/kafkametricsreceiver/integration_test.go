0a785d9a7ca34eb25724a9cf6266a501ed38e56f
james hughes <jameshughes@splunk.com>
2023-12-21 20:50:06 -0800

    remove skip in integ test

--- receiver/kafkametricsreceiver/integration_test.go
+++ receiver/kafkametricsreceiver/integration_test.go
@@ -26,8 +26,6 @@ const (
 )
 
 func TestIntegration(t *testing.T) {
-	t.Skip("Flaky Test - See https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/26293")
-
 	uid := fmt.Sprintf("-%s", uuid.NewString())
 	networkName := "kafka-network" + uid
 	zkContainerName := "zookeeper" + uid
